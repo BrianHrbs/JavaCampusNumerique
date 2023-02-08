67d163d34f5d4e67e85a96a4e93d4067c0d8f27c
B.Herbelles <brian.herbelles@le-campus-numerique.fr>
2023-02-08 16:19:35 +0100

    Exercice Demenagement Finish

--- src/Demenagement.java
+++ src/Demenagement.java
@@ -1,5 +1,30 @@
 public class Demenagement {
+
+    int count;
+    int maxcamion;
+
+    public Demenagement(int countactual, int maxcarton) {
+        count = countactual;
+        maxcamion = maxcarton;
+    }
+
     public static void main(String[] args) {
+        int carton = 3729;
+        int max = 9;
+
+        while(carton != 0) {
+            if (carton >= 9) {
+                carton = carton - max;
+                System.out.println("Le camion fait un voyage de " + max + " cartons");
+                System.out.println("Il reste " + carton + " au local");
+            } else {
+                System.out.println("Le camion fait un voyage de " + carton + " cartons");
+                carton = 0;
+                System.out.println("Il ne reste aucun carton au local");
+            }
+            System.out.println("___________________________________________________");
+        }
 
     }
 }
+
